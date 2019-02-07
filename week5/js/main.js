8eb3be8dfc9413b91d8a44f5658d6f87fa841992
visualstudio <visualstudio@230-PC28>
2019-02-07 11:43:42 -0500

    week 5

--- week5/js/main.js
+++ week5/js/main.js
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿function myFunction(a, b) {
+   
+    return a + b; //The function returns the product of a and b
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "green", "orange", "blue", "purple"];
+    console.log(pens);
+}
